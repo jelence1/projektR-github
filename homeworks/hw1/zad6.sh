3ea8179d9b14755dc407352c60003d280ef6209e
Jelena Gavran <jelena.gavran@fer.hr>
2021-11-02 21:19:48 +0100

    hw1

--- homeworks/hw1/zad6.sh
+++
@@ -1,9 +0,0 @@
-# Zadatak 6. - Napisati skriptu koja ce pronaci sve datoteke vece od
-# definirane velicine (preko argumenata) u “/home/username” direktoriju,
-#  ispisati i komprimirati u “backup.tgz”.
-
-n=""
-read -p "Upisi broj bajtova: " n
-n=n+"c"
-files=$(find /home/$(echo $USERNAME)/* -type f -size +${n}c)
-zip backup.tgz $files
